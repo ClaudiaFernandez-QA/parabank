4c194d8f6c1ce3ae98c35ab0893d7fa6e5f869dc
ClaudiaFernandez-QA <claudia.fernandez.qa@gmail.com>
2025-04-28 20:01:06 -0300

    feat: se agregó utilidades de generación de usuarios para los tests

---
+++ src/utils/generateUser.ts
@@ -0,0 +1,19 @@
+import { User } from '../data/user';
+
+export function generateNewUser(): User {
+  const randonNumber = Math.floor(Math.random() * 1000);
+  return {
+    firstName: 'Daenerys',
+    lastName: 'Targaryen',
+    address: 'Dracarys 123',
+    city: 'Dragonstone',
+    state: 'Crownlands',
+    zipCode: '12345',
+    phone: '1130556632',
+    ssn: '1212121212',
+    username: `user_${randonNumber}`,
+    password: 'TestPassword123'
+  };
+}
+//probandoando - probandoando123
+
